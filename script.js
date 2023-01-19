62851f9707b5fe63e56ccbebe0188e8f9566a102
Sundara krishna Penugonda <115982600+sundar38@users.noreply.github.com>
2023-01-19 12:40:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
-
+	let str1=str.toLowerCase();
+    let i=0;
+    let j=str.length()-1;
+    while(i!=j){
+        if(str1.charAt(i)!=str1.charAt(j)){
+            return false;
+        }
+    }
+    return true;
 }
 module.exports = palindrome
