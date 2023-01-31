feaa7d79ff67e6c73092493edb3ce04ccaa9b4cc
Sundara krishna Penugonda <115982600+sundar38@users.noreply.github.com>
2023-01-31 16:41:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function palindrome(str){
     // console.log(arr);
     let arrCopy = [...str];    // as reverse method also make changes to original array
     let arrReverse = arr.reverse();
-        // arr = arrCopy;
-    // console.log(`reverseArray:${arrReverse}`);
-    // console.log(`array:${arr}`);
+        arr = arrCopy;
+    console.log(`reverseArray:${arrReverse}`);
+    console.log(`array:${arr}`);
  
      for (let i = 0; i < arr.length; i++) {
         if (arrCopy[i] != arrReverse[i]) { 
