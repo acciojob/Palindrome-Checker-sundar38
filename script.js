eba6cd18c282372ce6253b5fc7d7e84358b4a853
Sundara krishna Penugonda <115982600+sundar38@users.noreply.github.com>
2023-01-31 10:16:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 // complete the given function
-
 function palindrome(str){
 	let str1=str.toLowerCase();
-    let i=0;
-    let j=str.length()-1;
+    var i=0;
+    var j=str.length()-1;
     while(i!=j){
         if(str1[i]!=str1[j]){
             return false;
